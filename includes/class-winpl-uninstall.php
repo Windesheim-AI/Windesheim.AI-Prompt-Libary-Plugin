676f20e3a588a93294c5b8d01bfe0dbecfad8baf
Tim Bentum <tim_bentum@live.nl>
2023-11-22 14:58:44 +0100

    feat: :sparkles: Remove prompt, pattern, and sector tables during uninstall

--- includes/class-winpl-uninstall.php
+++ includes/class-winpl-uninstall.php
@@ -5,6 +5,13 @@ class WinPL_Uninstall
 
     public static function uninstall()
     {
-        
+        global $wpdb;
+        $winPL_prompt_table = $wpdb->prefix . 'winpl_prompt';
+        $winPL_prompt_pattern_table = $wpdb->prefix . 'winpl_prompt_pattern';
+        $winPL_sector_table = $wpdb->prefix . 'winpl_sector';
+
+        $wpdb->query("DROP TABLE IF EXISTS $winPL_prompt_pattern_table");
+        $wpdb->query("DROP TABLE IF EXISTS $winPL_sector_table");
+        $wpdb->query("DROP TABLE IF EXISTS $winPL_prompt_table");
     }
 }
